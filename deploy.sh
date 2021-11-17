445e2827fd5551f5da5a4a94172e4f6d93dad542
Ubuntu <ubuntu@ip-172-31-17-253.ec2.internal>
2021-11-17 10:02:03 +0000

    added deplyment scripts

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,16 @@
-docker build -t hendrima/multi-client -f ./client/Dockerfile ./client
-docker build -t hendrima/multi-server -f ./server/Dockerfile ./server
-docker build -t hendrima/multi-worker -f ./worker/Dockerfile ./worker
+docker build -t hendrima/multi-client:latest -t hendrima/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t hendrima/multi-server:latest -t hendrima/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t hendrima/multi-worker:latest -t hendrima/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 dokcer push hendrima/multi-client
 dokcer push hendrima/multi-server
 dokcer push hendrima/multi-worker
+
+dokcer push hendrima/multi-client:$SHA
+dokcer push hendrima/multi-server:$SHA
+dokcer push hendrima/multi-worker:$SHA
+
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=hendrima/multi-server
+kubectl set image deployments/server-deployment server=hendrima/multi-server:$SHA
+kubectl set image deployments/client-deployment client=hendrima/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=hendrima/multi-worker:$SHA
 
