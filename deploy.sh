60eb98b2f5125ea7d851b6acb9c784927d29d26f
Ubuntu <ubuntu@ip-172-31-17-253.ec2.internal>
2021-11-17 10:41:44 +0000

    modifed deploy script to push image to docker hub

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,14 @@
 docker build -t hendrima/multi-client:latest -t hendrima/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t hendrima/multi-server:latest -t hendrima/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t hendrima/multi-worker:latest -t hendrima/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-dokcer push hendrima/multi-client
-dokcer push hendrima/multi-server
-dokcer push hendrima/multi-worker
 
-dokcer push hendrima/multi-client:$SHA
-dokcer push hendrima/multi-server:$SHA
-dokcer push hendrima/multi-worker:$SHA
+docker push hendrima/multi-client
+docker push hendrima/multi-server
+docker push hendrima/multi-worker
+
+docker push hendrima/multi-client:$SHA
+docker push hendrima/multi-server:$SHA
+docker push hendrima/multi-worker:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=hendrima/multi-server:$SHA
